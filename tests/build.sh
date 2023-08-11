5dfff3cac6c967f741b96bc8afe51663410ef241
Prem chavhan <pchavhan@logiquad.com>
2023-08-11 17:14:36 +0530

    Updated Fixes

--- tests/build.sh
+++ tests/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 chmod +x /dev/docker/Dockerfile
-cp -rf /dev/docker/* ./
+cp -rf dev/docker/* ./
 
 docker buildx build . --output type=docker,name=elestio4test/bookstack:latest | docker load
