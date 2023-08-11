1f855dcc2b0eb6243b38f6f4e2e79b6682a53397
Prem chavhan <pchavhan@logiquad.com>
2023-08-11 15:06:41 +0530

    Added tests folder

--- scripts/preInstall.sh
+++ scripts/preInstall.sh
@@ -1,2 +1,5 @@
+set env vars
+set -o allexport; source .env; set +o allexport;
+
 mkdir -p ./web
 chown -R 1001:1001 ./web
---
+++ tests/build.sh
@@ -0,0 +1,2 @@
+
+docker buildx build . --output type=docker,name=elestio4test/bookstack:latest | docker load
