b50e22698d0d89458d7a978b4b976cf816d28290
Prem chavhan <pchavhan@logiquad.com>
2023-08-11 16:02:36 +0530

    Pre-install Scripts Chnaged

--- scripts/preInstall.sh
+++ scripts/preInstall.sh
@@ -1,5 +1,12 @@
 set env vars
 set -o allexport; source .env; set +o allexport;
 
-mkdir -p ./web
-chown -R 1001:1001 ./web
+mkdir -p ./init.db
+mkdir -p db
+mkdir -p ./
+mkdir -p ./php/conf.d/xdebug.ini
+
+chown -R 1001:1001 ./init.db
+chown -R 1001:1001 db
+chown -R 1001:1001 ./
+chown -R 1001:1001 ./php/conf.d/xdebug.ini
