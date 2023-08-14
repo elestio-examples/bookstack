243e8fb701e3ef08694571ed1bd6fd609e06d790
Prem chavhan <pchavhan@logiquad.com>
2023-08-14 15:18:38 +0530

    Updated Fixes

--- scripts/preInstall.sh
+++ scripts/preInstall.sh
@@ -1,5 +1,11 @@
 set env vars
 set -o allexport; source .env; set +o allexport;
 
-mkdir -p ./
-chown -R 1001:1001 ./
+mkdir -p ./init.db
+mkdir -p ./db
+mkdir -p ./php/conf.d/xdebug.ini
+
+
+chown -R 1001:1001 ./init.db
+chown -R 1001:1001 ./db
+chown -R 1001:1001 ./php/conf.d/xdebug.ini
