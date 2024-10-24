27e79b48e9ee56b9fce24f5dbca32fa904ca18d0
Amit Shukla <53058928+amitshuklabag@users.noreply.github.com>
2024-10-24 16:10:28 +0530

    Update preInstall.sh

--- scripts/preInstall.sh
+++ scripts/preInstall.sh
@@ -10,3 +10,9 @@ chown -R 1001:1001 ./
 chown -R 1001:1001 ./init.db
 chown -R 1001:1001 ./db
 chown -R 1001:1001 ./php/conf.d/xdebug.ini
+
+APP_KEY=$(docker run -it --rm --entrypoint /bin/bash lscr.io/linuxserver/bookstack:latest appkey)
+
+cat << EOT >> ./.env
+APP_KEY=${APP_KEY}
+EOT
