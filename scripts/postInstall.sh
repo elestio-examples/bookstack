90e575dee6d71f5e901d42485ae2640a571ae3d5
Amit Shukla <53058928+amitshuklabag@users.noreply.github.com>
2024-10-24 16:07:32 +0530

    Update postInstall.sh

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -5,5 +5,5 @@ echo "Waiting for bookstack to be ready ...";
 sleep 20s;
 
 #Create admin user
-docker-compose exec -T bookstack_db mysql -u root -p${MYSQL_ROOT_PASSWORD} bookstackapp -e "DELETE FROM users WHERE id='1';"
+docker-compose exec -T db mysql -u root -p${MYSQL_ROOT_PASSWORD} bookstackapp -e "DELETE FROM users WHERE id='1';"
 docker-compose exec -T bookstack /bin/sh -c 'php /app/www/artisan bookstack:create-admin --email="'"${ADMIN_EMAIL}"'" --name="Admin" --password="'"${ADMIN_PASSWORD}"'"'
